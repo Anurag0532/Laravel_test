4d00f79457378147c08ec10127105ee75933f77b
Anurag <anuragchatterjee165@gmail.com>
2026-01-29 21:05:08 +0530

    First Commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\VerificationCodeMail;
+
+
+class AdminController extends Controller
+{
+    public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+    // End Method
+
+    public function AdminLogin(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+
+            $user = Auth::user();
+
+            $verificationCode = random_int(100000, 999999);
+            session(['verification_code' => $verificationCode, 'user_id' => $user->id]);
+
+            Mail::to($user->email)->send(new VerificationCodeMail($verificationCode));
+
+            Auth::logout();
+
+            return redirect()->route('custom.verification.form')->with("success", "A verification code has been sent to your email. Please enter the code to proceed.");
+
+        }
+
+        return redirect()->back()->withErrors(['email' => 'The provided credentials do not match our records.']);
+    }
+    // End Method
+
+    public function AdminProfile()
+    {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile', compact('profileData'));
+    }
+
+    public function ShowVerification()
+    {
+        return view('auth.verify');
+    }
+
+    public function VerificationVerify(Request $request)
+    {
+        $request->validate([
+            'code' => 'required|numeric',
+        ]);
+
+        if ($request->code == session('verification_code')) {
+            Auth::loginUsingId(session('user_id'));
+
+            session()->forget(['verification_code', 'user_id']);
+
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->withErrors(['code' => 'The provided verification code is incorrect.']);
+    }
+
+}
